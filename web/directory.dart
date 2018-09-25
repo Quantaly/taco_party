c5f2e01d1424af6f1869184d4c21d74208c045b6
Kai Page <kaibug@gmail.com>
2018-09-25 09:14:32 -0500

    Added message field to directory

---
+++ web/directory.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+
+final List<AnchorElement> links = [];
+
+const ids = ["default", "pokemon", "minediamonds", "energyihs", "tetris"];
+InputElement input;
+
+void main() {
+  input = querySelector("#msg") as InputElement;
+  print(input);
+  for (var id in ids) {
+    links.add(querySelector("#$id"));
+  }
+  input.onInput.listen((_) => links.forEach(updateHref));
+  links.forEach(updateHref);
+}
+
+void updateHref(AnchorElement element) {
+  var first = true;
+  String sep() {
+    if (first) {
+      first = false;
+      return "?";
+    }
+    return "&";
+  }
+  var builder = StringBuffer("./");
+  if (element.id != "default") builder.write("${sep()}type=${element.id}");
+  if (input.value.isNotEmpty) builder.write("${sep()}msg=${Uri.encodeComponent(input.value)}");
+  element.href = builder.toString();
+}
