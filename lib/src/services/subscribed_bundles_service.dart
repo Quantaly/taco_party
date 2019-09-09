dec953b8888cb75d501b6b5e25d567679dd4cf0e
Kai Page <kaibug@gmail.com>
2019-09-09 18:31:46 -0500

    New IndexedDB-based SubscribedBundlesService

--- lib/src/services/subscribed_bundles_service.dart
+++ lib/src/services/subscribed_bundles_service.dart
@@ -1,12 +1,122 @@
 import 'dart:collection';
 import 'dart:convert';
 import 'dart:html';
+import 'dart:indexed_db';
 
-class SubscribedBundlesService with ListMixin<String> {
+import 'package:angular/di.dart';
+
+const _defaultList = [
+  "https://quantaly.github.io/taco_party_official_bundle/bundle.yaml"
+];
+
+class SubscribedBundlesService {
+  static const subscriptionsStoreName = "subscriptions";
+
+  final Injector _injector;
+
+  SubscribedBundlesService(this._injector);
+
+  Future<_ServiceState> __stateFuture;
+  Future<_ServiceState> get _stateFuture =>
+      __stateFuture ?? (__stateFuture = _init());
+
+  // within methods of this class, when you have a state, use state.blocked
+  Future<bool> get blocked async => (await _stateFuture).blocked;
+
+  Stream<String> getSubscriptions() async* {
+    final state = await _stateFuture;
+    if (state.blocked) {
+      yield* Stream.fromIterable(_defaultList);
+    } else {
+      await for (var cursor in state.db
+          .transaction(subscriptionsStoreName, "readonly")
+          .objectStore(subscriptionsStoreName)
+          .openCursor()) {
+        yield cursor.value["url"];
+        cursor.next();
+      }
+    }
+  }
+
+  Future<bool> subscribedTo(String url) async {
+    final state = await _stateFuture;
+    if (state.blocked) {
+      return false;
+    } else {
+      return await state.db
+              .transaction(subscriptionsStoreName, "readonly")
+              .objectStore(subscriptionsStoreName)
+              .getObject(url) !=
+          null;
+    }
+  }
+
+  Future<void> subscribe(String url) async {
+    final state = await _stateFuture;
+    if (!state.blocked) {
+      if (!await subscribedTo(url)) {
+        final subscriptionsStore = state.db
+            .transaction(subscriptionsStoreName, "readwrite")
+            .objectStore(subscriptionsStoreName);
+        await subscriptionsStore.add({"url": url});
+      }
+    }
+  }
+
+  Future<void> unsubscribe(String url) async {
+    final state = await _stateFuture;
+    if (!state.blocked) {
+      final subscriptionsStore = state.db
+          .transaction(subscriptionsStoreName, "readwrite")
+          .objectStore(subscriptionsStoreName);
+      await subscriptionsStore.delete(url);
+    }
+  }
+
+  static const _dbVersion = 1;
+  Future<_ServiceState> _init() async {
+    print("_init ran");
+    var blocked = false;
+    final db = await window.indexedDB.open(
+      "taco_party",
+      version: _dbVersion,
+      onUpgradeNeeded: _upgradeDb,
+      onBlocked: (_) {
+        blocked = true;
+      },
+    );
+    print("blocked is $blocked");
+    return _ServiceState(db, blocked);
+  }
+
+  void _upgradeDb(VersionChangeEvent evt) {
+    final Database db = evt.target.result;
+    for (var currentVersion = evt.oldVersion;
+        currentVersion < evt.newVersion;
+        currentVersion++) {
+      switch (currentVersion) {
+        case 0:
+          final subscriptionsStore =
+              db.createObjectStore(subscriptionsStoreName, keyPath: "url");
+          final OldSubscribedBundlesService oldService =
+              _injector.get(OldSubscribedBundlesService);
+          for (final url in oldService) {
+            subscriptionsStore.add({"url": url});
+          }
+          break;
+      }
+    }
+  }
+}
+
+class _ServiceState {
+  final Database db;
+  final bool blocked;
+  _ServiceState(this.db, this.blocked);
+}
+
+class OldSubscribedBundlesService with ListMixin<String> {
   static const _storageKey = "taco_party:subscribedBundles";
-  static const _defaultList = [
-    "https://quantaly.github.io/taco_party_official_bundle/bundle.yaml"
-  ];
 
   List<String> get _storedList {
     if (!window.localStorage.containsKey(_storageKey)) {
